a0186823c796e6c1c398e5c5abb94a8325d2dd09
Jacob Martinez-Troxel <jacobmtroxel@gmail.com>
2025-09-22 14:44:26 -0700

    Adding extensions and syncthing service to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,17 @@
+echo "Updating and installing packages..."
 sudo dnf update -y
-sudo dnf install -y gnome-tweaks fzf fd-find stow zoxide
+sudo dnf install -y gnome-tweaks fzf fd-find stow zoxide syncthing
 
+# deploy configs
+echo "Deploying config files"
 stow .
+
+# enable AppIndicatorSupport extension
+echo "Enabled gnome extensions"
+gsettings set org.gnome.shell disable-user-extensions false
+gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+
+# register syncthing as a user service
+echo "Enabled syncthing service"
+systemctl --user enable syncthing.service
+systemctl --user start syncthing.service
